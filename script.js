cbe913843dd129f9fa0322a2ec82b33db74961e3
pamela-martinez <72035155+pamela-martinez@users.noreply.github.com>
2021-06-08 13:22:51 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+$(".spell-one").click(function() {
+    $(".rabbit").show();
+    
+});
+
+$("h1").click(function() {
+    $(".wizard").toggle();
+});
+
+$(".spell-three").click(function() {
+    $("p").text ("willy billy pow!");
+    $("p").css("background", "red");
+    
+});
